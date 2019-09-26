6eff08e88a13ab5c9b6b5066a364fdb0ba0ebc3d
robert <xiongfeng@funtsui.com>
2019-09-26 19:03:44 +0800

    加信息

--- src/main/java/com/robert/sample/Client.java
+++ src/main/java/com/robert/sample/Client.java
@@ -1,13 +1,18 @@
 package com.robert.sample;
 
+import com.robert.sample.client.TcpClient;
 import com.robert.sample.client.UDPSearcher;
 import com.robert.sample.client.bean.ServerInfo;
+import com.robert.util.PrintUtil;
 
 public class Client {
 
     public static void main(String[] args) {
-
         ServerInfo serverInfo = UDPSearcher.searchServer(3000);
-        System.out.println(serverInfo);
+        if (serverInfo != null) {
+            TcpClient.startConnect(serverInfo);
+        } else {
+            PrintUtil.println("未查找到server");
+        }
     }
 }
--- src/main/java/com/robert/sample/client/TcpClient.java
+++ src/main/java/com/robert/sample/client/TcpClient.java
@@ -1,42 +1,60 @@
 package com.robert.sample.client;
 
+import com.robert.sample.client.bean.ServerInfo;
 import com.robert.util.CloseUtils;
+import com.robert.util.PrintUtil;
 
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.net.SocketException;
+import java.net.SocketTimeoutException;
 
 public class TcpClient {
 
-    private final int port;
-    private final String host;
-    private Socket socket;
 
-    public TcpClient(String host, int port) {
-        this.host = host;
-        this.port = port;
-    }
-
-    public boolean connect() {
+    public static void startConnect(ServerInfo serverInfo) {
+        ReadHandler readHandler = null;
+        Socket socket = null;
         try {
-            socket = new Socket(host, port);
-            ReadHandler readHandler = new ReadHandler(socket.getInputStream());
+            socket = new Socket();
+//            socket.setSoTimeout(3000);
+            socket.connect(new InetSocketAddress(serverInfo.getAddress(), serverInfo.getPort()));
+            PrintUtil.println("已发起服务器连接，并进入后续程序");
+            PrintUtil.println("客户端信息： " + socket.getLocalAddress() + ", p: " + socket.getLocalPort());
+            PrintUtil.println("服务端信息：" + socket.getInetAddress() + "， P:" + socket.getPort());
+            readHandler = new ReadHandler(socket.getInputStream());
             readHandler.start();
-
-            return true;
+            write(socket);
         } catch (IOException e) {
             e.printStackTrace();
+            PrintUtil.println("客户端异常退出。。。");
+        } finally {
+            if (readHandler != null) {
+                readHandler.exit();
+            }
+            CloseUtils.close(socket);
+            PrintUtil.println("客户端退出");
         }
-        return false;
     }
 
-    public void exit() {
-        CloseUtils.close(socket);
+    private static void write(Socket socket) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String result = "";
+        PrintStream printStream = new PrintStream(socket.getOutputStream());
+        do {
+            result = reader.readLine();
+            printStream.println(result);
+        } while (!"00bye00".equalsIgnoreCase(result));
+        CloseUtils.close(printStream);
+        CloseUtils.close(reader);
     }
 
-    private final class ReadHandler extends Thread {
+    private final static class ReadHandler extends Thread {
 
         private final InputStream inputStream;
+        private boolean done = false;
 
         public ReadHandler(InputStream inputStream) {
             this.inputStream = inputStream;
@@ -44,8 +62,33 @@ public class TcpClient {
 
         @Override
         public void run() {
+            try {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+                do {
+                    String msg = null;
+                    try {
+                        msg = reader.readLine();
+                    } catch (SocketTimeoutException e) {
+                        e.printStackTrace();
+                    }
+                    if (msg == null) {
+                        System.out.println("连接已关闭，无法读取数据~");
+                        break;
+                    }
+                    PrintUtil.println("客户端接收到消息：%s", msg);
+                } while (!done);
+            } catch (Exception e) {
+                if (!done) {
+                    PrintUtil.println("连接异常断开" + e.getMessage());
+                }
+            } finally {
+                CloseUtils.close(inputStream);
+            }
+        }
 
-
+        public void exit() {
+            done = true;
+            CloseUtils.close(inputStream);
         }
     }
 
